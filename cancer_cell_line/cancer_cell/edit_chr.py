27949af1ae9c9a15dc7354d5f077b13bac9000e0
NeilHumps <humphryesna@gmail.com>
2015-07-06 15:16:17 +0100

    cancer cell line code with cli function

--- cancer_cell_line/cancer_cell/edit_chr.py
+++ cancer_cell_line/cancer_cell/edit_chr.py
@@ -1,7 +1,12 @@
 import subprocess
 import os
+import click
 
 from dgl import library_constants
+
+@click.group()
+def cli():
+    pass
 
 def fasta_single_seq_parser(fasta_file, fasta_dir):
     """
@@ -24,13 +29,11 @@ def fasta_single_seq_parser(fasta_file, fasta_dir):
         fasta_seq += line.strip()
     return fasta_seq
 
-
-def add_snps(fasta_file, chrom, snp_vcf, chrom_dir):
+def add_snps(fasta, chrom, snp_vcf):
     """
     use caveman.vcf-type file to apply SNPs to
     chromosome sequence
     """
-    fasta = fasta_single_seq_parser(fasta_file, chrom_dir)
     dodgy = []
     with open(snp_vcf) as file:
         contents = file.readlines()
@@ -47,57 +50,68 @@ def add_snps(fasta_file, chrom, snp_vcf, chrom_dir):
                         snp[1],
                         snp[2],
                         fasta[int(snp[1])-1]))
-    with open('chr{}_snps.flat'.format(chrom), 'w') as fasta_file:
-        fasta_file.write('>SNPed_chr{}\n{}\n'.format(
-                            chrom,
-                            fasta,))
+    #with open('snps_chr.{}.fasta'.format(chrom), 'w') as fasta_file:
+    #    fasta_file.write('>SNPed_chr{}\n{}\n'.format(
+    #                        chrom,
+    #                        fasta,))
     if len(dodgy) > 0:
         with open('incorrect_snps.txt', 'a') as error_file:
             error_file.write('\n'.join(dodgy) + '\n')
+    return fasta
 
-def map_indels(chr_fasta, chr, genome, indel_table, outfile_prefix):
+def add_indels_with_mapping(fasta, chrom, indel_vcf):
     """
     use pindel.vcf-type file to integrate INDELs into
     chromosome sequence and make a  cumulative map file
     to adjust feature coordinates.
     """
-    fasta = str(SeqIO.read(chr_fasta, 'fasta').seq)
-    with open(indel_table) as file:
+    with open(indel_vcf) as file:
         contents = file.readlines()
-    indels = [line.strip().split('\t') for line in contents if line.startswith(chr + '\t') == True]
+    indels = [line.strip().split('\t') for line in contents
+              if line.startswith(chrom + '\t') == True]
     map = []
     dodgy = []
     # start from end of chromosome to retain coordinates
     for indel in reversed(indels):
+        # ensure cooordinate is within chromosome length
         if int(indel[1]) <= len(fasta):
-            if fasta[int(indel[1])-1] == indel[3][0]:
-                fasta = fasta[0:int(indel[1])-1] + indel[4] + fasta[(int(indel[1])-1) + len(indel[3]):]
-                map.append([indel[0], indel[1], str(len(indel[4]) - len(indel[3]))])
+            indel_start = int(indel[1]) -1
+            indel_end = int(indel[1]) + len(indel[3])
+            if fasta[indel_start:indel_end] == indel[3]:
+                fasta = fasta[:indel_start] + indel[4] + fasta[indel_end - 1:]
+                map.append([indel[0],
+                            indel[1],
+                            str(len(indel[4]) - len(indel[3]))
+                            ]
+                           )
             else:
                 dodgy.append(
-                'incorrect reference sequence: ' + indel[0] + ' ' + indel[1] + ' ' + indel[2] + ' ' + fasta[int(indel[1])-1])
-    fasta_file = open(outfile_prefix + '.fasta', 'w')
-    fasta_file.write('>INDELed_chr' + chr + '\n' + fasta + '\n')
-    fasta_file.close()
+                'incorrect reference sequence: chr{} @{} VCF:{} fasta:{}'.format(
+                        indel[0],
+                        indel[1],
+                        indel[2],
+                        fasta[indel_start:indel_end]))
+    #with open('snps_indels_chr.{}.fasta'.format(chrom), 'w') as fasta_file:
+    #    fasta_file.write('>SNPed_INDELed_chr{}\n{}\n'.format(chrom, fasta))
     if len(dodgy) > 0:
-        error_file = open('chr' + chr + '_' + genome + '_incorrect_INDELs.txt', 'w')
-        error_file.write('\n'.join(dodgy))
-        error_file.close()
+        with open('incorrect_indels.txt', 'a') as error_file:
+            error_file.write('\n'.join(dodgy) + '\n')
     map_cum = []
     for change in reversed(map):
         if len(map_cum) == 0:
             map_cum.append(change)
         else:
-            map_cum.append([change[0], change[1], str(int(map_cum[len(map_cum)-1][2]) + int(change[2]))])
+            map_cum.append([change[0],
+                            change[1],
+                            str(int(map_cum[len(map_cum)-1][2]) + int(change[2]))
+                            ]
+                           )
     map_line = ['\t'.join(entry) for entry in map_cum]
-    map_file = open(outfile_prefix + '.map', 'w')
-    map_file.write('\n'.join(map_line))
-    map_file.close()
+    with open('indel_map.txt', 'a') as map_file:
+        map_file.write('\n'.join(map_line) + '\n')
+    return fasta
 
-
-
-
-def add_snps_indels_cli(snp_file, indel_file, chromosome_constant, chrom_dir):
+def add_snps_indels(snp_vcf, indel_vcf, chromosome_constant, chrom_dir, genome_name):
     chromosomes = getattr(library_constants, chromosome_constant)
     chrom_files = os.listdir(chrom_dir)
     for chrom in chromosomes:
@@ -106,20 +120,32 @@ def add_snps_indels_cli(snp_file, indel_file, chromosome_constant, chrom_dir):
         if len(chrom_file) != 1:
             raise error('chromosome files are named incorrectly')
         else:
-            add_snps(chrom_file[0], chrom, snp_vcf, chrom_dir)
+            fasta_seq = fasta_single_seq_parser(chrom_file[0],
+                                                chrom_dir)
+            snped_fasta_seq = add_snps(fasta_seq,
+                                       chrom,
+                                       snp_vcf)
+            indeled_fasta_seq = add_indels_with_mapping(snped_fasta_seq,
+                                                        chrom,
+                                                        indel_vcf)
+        with open('{}_ref_with_snps-indels.Chromosome.{}.fasta'.format(
+                    genome_name,
+                    chrom
+                    )) as outfile:
+            outfile.write('>{}_chr{}\n{}\n'.format(
+                            genome_name,
+                            chrom,
+                            indeled_fasta_seq
+                            ))
 
+@cli.command()
+@click.argument('snp_vcf')
+@click.argument('indel_vcf')
+@click.argument('chromosome_constant')
+@click.argument('chrom_dir')
+@click.argument('genome_name')
+def add_snps_indels_cli(snp_vcf, indel_vcf, chromosome_constant, chrom_dir, genome_name):
+    add_snps_indels(snp_vcf, indel_vcf, chromosome_constant, chrom_dir, genome_name)
 
-
-
-# chr_fasta = 'Homo_sapiens.GRCh37.75.dna_rm.chromosome.20.fa'
-# snp_table = 'A375_caveman.vcf'
-# chr_fasta = 'chr20_SNPed.fasta'
-# indel_table = 'A375_pindel.vcf'
-
-# edit_chr.add_snps('Homo_sapiens.GRCh37.75.dna_rm.chromosome.20.fa', '20', 'GRCh37', 'A375_caveman.vcf', 'chr20_GRCh37_SNPed.fasta')
-# edit_chr.map_indels('chr20_GRCh37_SNPed.fasta', '20', 'GRCh37', 'A375_pindel.vcf', 'chr20_GRCh37_INDELs')
-
-# edit_chr.add_snps('Homo_sapiens.GRCh38.dna_rm.chromosome.20.fa', '20', 'GRCh38', 'A375_caveman.vcf', 'chr20_GRCh38_SNPed.fasta')
-# edit_chr.map_indels('chr20_GRCh38_SNPed.fasta', '20', 'GRCh38', 'A375_pindel.vcf', 'chr20_GRCh38_INDELs')
-
-
+if __name__ == '__main__':
+    cli()
