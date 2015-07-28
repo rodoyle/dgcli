8437b4bad7ca9b2006a820f40b81513d697fbbad
Riley Doyle <rileyd@deskopgenetics.com>
2015-07-28 21:52:55 +0100

    overhauled inventory service

--- dgcli/inventory.py
+++ dgcli/inventory.py
@@ -7,15 +7,11 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import json
 import logging
 import os
 import re
-import yaml
-from collections import namedtuple
 
 import requests
-from click import echo
 import dgparse
 
 log = logging.getLogger(__name__)
@@ -44,22 +40,57 @@ RELATIONS = {
 }
 
 
+class AuthenticationError(Exception):
+    pass
+
+
 class InventoryService(object):
     """
     Represents the Inventory Service and its public methods.
     """
 
-    def __init__(self, target_server, credentials, on_success, on_error):
+    def __init__(self, target_server):
         self.target_server = target_server
-        self.credentials = credentials
-        self.on_success = on_success
-        self.on_error = on_error
         self.crud_url = os.path.join(self.target_server, 'api/inventory/crud')
         self.upload_url = os.path.join(self.target_server, 'api/inventory/upload')
+        self.login_url = os.path.join(self.target_server, 'api/inventory/authenticate')
+        self.session = requests.Session()
 
-    def create(record):
+    def set_credentials(self, username, password):
+        self.session.auth = (username, password)
+
+    def _get_schema(self, record):
+        type_ = record.get('type_')
+        return dgparse.VALIDATORS[type_]
+
+    def login(self, username, password):
+        self.set_credentials(username, password)
+        resp = self.session.post(self.login_url, {})
+        # Copy Session Token
+        if resp.ok:
+            self.session.cookies['session'] = resp.cookies['session']
+        else:
+            raise AuthenticationError
+
+    def create(self, record):
         """Creates a record"""
-        resp = requests.post(endpoint, dgparse.schema, auth=credentials)
+        schema = self._get_schema(record)
+        body, errors = schema.dumps(record)
+        if errors:
+            return record, errors
+        resp = self.session.post(self.crud_url, json=body)
+        if resp.ok:
+            return resp.json()['read'], {}
+        else:
+            try:
+                return record, resp.json()
+            except ValueError:
+                return record, {'create': resp.text}
+
+
+def construct_service(config):
+    inv_serve = InventoryService(config.target_server)
+    return inv_serve
 
 
 
